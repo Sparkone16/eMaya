e5e35b529a49011cbd98b62d3c30d9d90a05d557
Leandro <leandroveyrenc@gmail.com>
2026-03-17 15:50:42 +0100

    Update

---
+++ lib/src/features/produit/presentation/widgets/produit_promotion_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProduitPromotionWidget extends StatefulWidget {
+  const ProduitPromotionWidget({super.key});
+
+  @override
+  State<ProduitPromotionWidget> createState() => _ProduitPromotionWidgetState();
+}
+
+class _ProduitPromotionWidgetState extends State<ProduitPromotionWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Text('promotion')
+    );
+  }
+}
---
+++ lib/src/features/produit/presentation/widgets/produit_vente_widget.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ProduitVenteWidget extends StatefulWidget {
+  const ProduitVenteWidget({super.key});
+
+  @override
+  State<ProduitVenteWidget> createState() => _ProduitVenteWidgetState();
+}
+
+class _ProduitVenteWidgetState extends State<ProduitVenteWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+      child: Text('vente'),
+    );
+  }
+}
